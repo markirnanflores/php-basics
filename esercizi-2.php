4447d0a6c08c5c27fefdee8ff05d613a673736c3
Mark Flores <38152455+markirnanflores@users.noreply.github.com>
2022-11-23 09:05:48 +0100

    Aggiunta soluzione

--- esercizi-2.php
+++ esercizi-2.php
@@ -30,3 +30,35 @@ echo 'Il numero più grande è ' . $numeroGrande . PHP_EOL;
 echo 'Il numero più grande è ripetuto n. ' . conteggioNumero($numeri, $numeroGrande)  . ' volte' . PHP_EOL;
 echo 'Il numero più grande è nelle seguenti posizioni ' . PHP_EOL;
 print_r(posizioniNumero($numeri, $numeroGrande));
+
+
+/* Soluzione */
+// function conteggioNumero($array, $numero): int
+// {
+//     $i=0;
+//     $contatore = 0;
+    
+//     while ($i < count($array)) {
+//         if ($array[$i] == $numero) {
+//             $contatore = $contatore + 1;
+//         }
+//         $i++;
+//     }
+    
+//     return $contatore;
+// }
+
+// function posizioniNumero($array, $numero): array
+// {
+//     $i=0;
+//     $posizioni = [];
+
+//     while ($i < count($array)){
+//         if($array[$i] == $numero){
+//             array_push($posizioni,$i);
+//         }
+//         $i++;
+//     }
+    
+//     return $posizioni;
+// }
